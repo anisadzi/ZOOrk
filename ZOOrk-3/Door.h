80f145d6353b51f3dc0d95ebbefed11c955b3559
anisadzi <90449593+anisadzi@users.noreply.github.com>
2024-06-13 09:42:29 +0700

    Implement Door from a new class

---
+++ ZOOrk-3/Door.h
@@ -0,0 +1,27 @@
+#ifndef ZOORK_DOOR_H
+#define ZOORK_DOOR_H
+
+#include "Passage.h"
+#include "Item.h"
+#include "Player.h"
+
+class Door : public Passage {
+public:
+    Door(const std::string &n, const std::string &d, Room* from, Room* to, const Item &requiredItem)
+        : Passage(n, d, from, to), requiredItem(requiredItem) {}
+
+    void enter() override {
+        Player* player = Player::instance();
+        if (player->hasItem(requiredItem.getName())) {
+            std::cout << "You use the " << requiredItem.getName() << " to unlock the door.\n";
+            Passage::enter();
+        } else {
+            std::cout << "The door is locked. You need a " << requiredItem.getName() << " to open it.\n";
+        }
+    }
+
+private:
+    Item requiredItem;
+};
+
+#endif //ZOORK_DOOR_H
---
+++ ZOOrk-3/Room.h
@@ -0,0 +1,55 @@
+//
+// Created by Richard Skarbez on 5/7/23.
+//
+
+#ifndef ZOORK_ROOM_H
+#define ZOORK_ROOM_H
+
+#include "Character.h"
+#include "RoomDefaultEnterCommand.h"
+#include "Item.h"
+#include "Location.h"
+#include <map>
+#include <algorithm>
+
+class Passage;
+
+class Room : public Location {
+public:
+    Room(const std::string &, const std::string &);
+    Room(const std::string &, const std::string &, std::shared_ptr<Command>);
+
+    void addItem(const Item& item);
+    void removeItem(const std::string& itemName);
+    bool hasItem(const std::string& itemName) const;
+    bool hasItems() const;
+    Item takeItem(const std::string& itemName);
+    void listItems() const;
+    void printInventory() const;
+
+
+//    void addCharacter(Character*);
+//    void removeCharacter(const std::string&);
+//    Character* getCharacter(const std::string&);
+
+    void addPassage(const std::string &, std::shared_ptr<Passage>);
+    void removePassage(const std::string &);
+    std::shared_ptr<Passage> getPassage(const std::string &);
+
+    std::string getDescription() const;
+
+protected:
+//    std::vector<Item*> items;
+//    std::vector<Character*> characters;
+    std::map<std::string, std::shared_ptr<Passage>> passageMap;
+
+private:
+    std::string name;
+    std::string description;
+    std::vector<Item> items;
+    std::vector<std::string> inventory;
+};
+
+
+
+#endif //ZOORK_ROOM_H
