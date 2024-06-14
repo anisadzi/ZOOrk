64c96127e517c8de1e3018eddd0c2b5ff3de100c
anisadzi <90449593+anisadzi@users.noreply.github.com>
2024-06-14 16:21:25 +0700

    Delete ZOOrk-2 directory

--- ZOOrk-2/NullCommand.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Richard Skarbez on 5/7/23.
-//
-
-#include "NullCommand.h"
-
-void NullCommand::execute() {
-    std::cout << "Nothing happens.\n";
-}
--- ZOOrk-2/RoomDefaultEnterCommand.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Richard Skarbez on 5/7/23.
-//
-
-#include "RoomDefaultEnterCommand.h"
-
-void RoomDefaultEnterCommand::execute() {
-    std::cout << gameObject->getDescription() << "\n";
-}
